a9ce0aa7135cb0ea39045bac7791094ed40d04a2
Josh <josh@creativeworks.com.au>
2025-05-23 19:03:04 +0800

    Added saving func

--- Toolbox/interferometerControlCode.py
+++ Toolbox/interferometerControlCode.py
@@ -5,25 +5,63 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 import pickle
 import pandas as pd
+import scipy as sp
+from scipy.signal import savgol_filter, find_peaks
+from scipy.special import j0, j1
+from scipy.ndimage import gaussian_filter1d
 
 from mokuProControl import *
 from kinesisMotorControl import *
 from uITLA.uITLAControl import *
-from myTools import movingAverage
+from saving import *
+from myTools import movingAverage, findNearest
+from visibilityTools import getVisibility
 
 def quit(moku=None, motor=None, laser=None):
     if moku != None: quitMoku(moku)
     if motor != None: quitMotor(motor)
     if laser != None: turnOffLaser(laser)
+    
+def removeOutliers(data1, data2, exclusion=0.5):
+    data_sum = data1 + data2
+    avg_tot = np.average(data_sum)
+    
+    avg1 = np.average(data1)
+    data1[data_sum < avg_tot*(1-exclusion)] = avg1
+    data1[data_sum > avg_tot*(1+exclusion)] = avg1
+    
+    avg2 = np.average(data2)
+    data2[data_sum < avg_tot*(1-exclusion)] = avg2
+    data2[data_sum > avg_tot*(1+exclusion)] = avg2
 
-def getMidPoint(data):
-    data[data < 1e3] = np.average(data)
-    data[data > 2e6] = np.average(data)
-    data = np.abs(data - np.average(data))
-    total_len = len(data)
-    data = movingAverage(data, int(len(data)/1000))
-    return int(np.argmax(data)*(total_len/len(data)))
+    data_mid = data1
+    data_mid = np.abs(data1 - np.average(data1))
+    total_len = len(data_mid)
+    data_mid = movingAverage(data_mid, int(len(data_mid)/1000))
+    mid = int(np.argmax(data_mid)*(total_len/len(data_mid)))
+    return data1, data2, mid
 
+def findVis(data):
+    slc = gaussian_filter1d(data, sigma=2) # filter the peaks the remove noise,
+    peaks = find_peaks(slc)[0] # [0] returns only locations 
+    troughs = find_peaks(-slc)[0] # [0] returns only locations 
+
+    max_pos = slc[peaks].argmax() # index of peaks that gives the index with the peak (very confusing I know)
+    min_pos = findNearest(troughs, peaks[max_pos])
+    
+    max_val = data[peaks[max_pos]]
+    
+    if troughs[min_pos] > peaks[max_pos]:
+        min_val = (data[troughs[min_pos]] + data[troughs[min_pos-1]])/2
+        used_vals = [troughs[min_pos-1], peaks[max_pos], troughs[min_pos]]
+    else:
+        min_val = (data[troughs[min_pos]] + data[troughs[min_pos+1]])/2
+        used_vals = [troughs[min_pos], peaks[max_pos], troughs[min_pos+1]]
+
+    print(max_val, min_val)
+    visibility = np.abs(getVisibility(max_val, min_val))
+    return visibility, used_vals
+        
 def menloDataRun(uITLA = False): 
     myLaser = turnOnLaser() if uITLA else None # chuck this at the beginning any time there is a laser
     
@@ -84,7 +122,7 @@ def menloDataRun(uITLA = False):
     
     print('Plot finished')
 
-def snspdMeasure(saving=False, filename=''):    
+def snspdMeasure(saving=False, source_size='200um', dist='60mm', baseline='127um'):    
     m, tfa, osc = initialisePersistMokuPro()
     motor = initialiseMotor("26003312")
     
@@ -122,6 +160,11 @@ def snspdMeasure(saving=False, filename=''):
         data1 = np.concatenate((data1, a)) # CH3 (green)
         data2 = np.concatenate((data2, b)) # CH4 (yellow)
     
+    if len(data1) > len(data2):
+        data1 = data1[:len(data2)]
+    else:
+        data2 = data2[:len(data1)]
+        
     ch3_offset, ch4_offset = 0, 0
     count_to_signal = 100e-6 # 100e-6 is for 100uV / count
     signal_buckets = 1e-2 # 10ms buckets
@@ -132,42 +175,88 @@ def snspdMeasure(saving=False, filename=''):
     #data1 = data1[(min_allowed<data1)&(data1<max_allowed)]
     #data2 = data2[(min_allowed<data2)&(data2<max_allowed)]
     
-    length = np.min([len(data1), len(data2)])
-    averaging_no = int((length/total_time)*signal_buckets / 5) # total number of points / total time = data rate -> data rate * signal buckets = number of points that should be around the same, the 5 is just an extra offset value
+    length = len(data1)
+    # total number of points / total time = data rate -> data rate * signal buckets = number of points that should be around the same, the 5 is just an extra offset value
+    averaging_no = int((length/total_time)*signal_buckets / 5) 
     print('Averaging: {} points (from {} total points)'.format(averaging_no, len(data1)))
     
     dividable_len = int(length//averaging_no * averaging_no)
     data1 = np.average(data1[0:dividable_len].reshape(-1, averaging_no), axis=1)
     data2 = np.average(data2[0:dividable_len].reshape(-1, averaging_no), axis=1)
     
-    length = np.min([len(data1), len(data2)])
+    length = len(data1)
     print('Final len: {}'.format(length))
     positions = np.linspace(first_pos, last_pos, length)*2e3 # Converted to mm path length added
     
-    mid = getMidPoint(data1) #TODO: should probably change this to a rolling average
-    positions = positions - positions[mid] # Finding the fringe peak
+    
+    modified_data1, modified_data2, mid_index = removeOutliers(data1, data2, exclusion= 0.5)
+    #mid_index = getMidPoint(data1, data2, exclusion=0.5) #TODO: should probably change this to a rolling average
+    positions = positions - positions[mid_index] # Finding the fringe peak
+    
+    # finding max and min
+    data1_vis, valsUsed1 = findVis(modified_data1)
+    data2_vis, valsUsed2 = findVis(modified_data2)
+    
+    print('Visibility from data1: {}, from data2: {}'.format(data1_vis, data2_vis))
     
     osc.enable_rollmode(True)
     quit(moku=osc, motor=motor)
     
     if saving:
-        #dbfile = open('{}.pkl'.format(filename), 'ab')
-        #pickle.dump({'Outputs1':data1, 'Outputs2':data2, 'Positions':positions}, dbfile)
+        #dbfile = open('{}.pkl'.format(filename), 'ab') #pickle.dump({'Outputs1':data1, 'Outputs2':data2, 'Positions':positions}, dbfile)
         all_outs = np.array([data1, data2, positions]).T
         headers = ['Output1', 'Output2', 'Positions']
-        df = pd.DataFrame(all_outs, columns=headers)
-        df.to_csv('{}.csv'.format(filename), header=True, sep=',')
+        df = pd.DataFrame(all_outs, columns=headers) #df.to_csv('{}.csv'.format(filename), header=True, sep=',')
+        metadata = generateMetadata('LED', source_size, dist, baseline, pol=0, parts=[], params = {'snspd integration':'10ms', 'volt per count':'100uV/cnt', 'moku inputs':'DC 1Mohm 400mVpp', 'measured vis':np.max([data1_vis, data2_vis])})
+        save('interferometer', df, metadata)
     
     print('Plotting')
     plt.figure(0)
-    plt.plot(positions, data1[:length], label='SNSPD 1') # the x2 for all these is because the beam is reflected
-    plt.plot(positions, data2[:length], label='SNSPD 2')
+    plt.plot(positions, modified_data1, label='SNSPD 1') # the x2 for all these is because the beam is reflected
+    plt.plot(positions, modified_data2, label='SNSPD 2')
+    plt.plot(positions[valsUsed1], data1[valsUsed1], marker='o', label='Vals Used 1')
+    plt.plot(positions[valsUsed2], data2[valsUsed2], marker='o', label='Vals Used 2')
     plt.xlabel('Path length difference')
     plt.ylabel('Counts')
-    plt.title('Counts vs Position')
-    plt.ylim([0, 2e6])
+    plt.title('Counts vs Position (outliers removed)')
+    plt.ylim([0, 1e6])
     plt.legend()
     plt.show()
 
+def testingLoad(campaign):
+    df, metadata = load(campaign, 1)
+    
+    data1 = np.array(df['Output1'])
+    data2 = np.array(df['Output2'])
+    positions = np.array(df['Positions'])
+    data1, data2, mid_index = removeOutliers(data1, data2, exclusion=0.3)
+    positions = positions - positions[mid_index]
+    data1_vis, valsUsed1 = findVis(data1)
+    data2_vis, valsUsed2 = findVis(data2)
+    print(data1_vis, data2_vis)
+    print(data1[valsUsed1])
+    print(data2[valsUsed2])
+    
+    print('Plotting')
+    plt.figure(0)
+    
+    #plt.plot(positions, gaussian_filter1d(data1, sigma=1), label='SNSPD 1 smoothed') # the x2 for all these is because the beam is reflected
+    plt.plot(positions, data1, label='SNSPD 1') # the x2 for all these is because the beam is reflected
+    plt.plot(positions, data2, label='SNSPD 2')
+    plt.plot(positions[valsUsed1], data1[valsUsed1], marker='o', label='Vals Used 1')
+    plt.plot(positions[valsUsed2], data2[valsUsed2], marker='o', label='Vals Used 2')
+    plt.xlabel('Path length difference')
+    plt.ylabel('Counts')
+    plt.title('Counts vs Position')
+    plt.ylim([0, 1e6])
+    plt.legend()
+    plt.show()
+    
 
-snspdMeasure(saving=False, filename='testing')
+#testingLoad('interferometer')
+#motor = initialiseMotor("26003312")
+#moveMotor(motor, pos=0e-6, acc=1e-3, max_vel=1e-3, delay=0)
+#motor.wait_move() # Move to start
+#print('Moved')
+
+snspdMeasure(saving=True, source_size='500um')
---
+++ Toolbox/saving.py
@@ -0,0 +1,95 @@
+# Saving and loading data, generalised format
+import os
+import sys
+import inspect
+import pandas as pd
+import numpy as np
+import yaml
+import glob
+import pyarrow as pa
+import pyarrow.parquet as pq
+from datetime import datetime
+
+def generateMetadata(source, source_size, dist, baseline, pol = None, parts = [], params = {}):
+    """_summary_
+
+    Args:
+        source (str): Source type, i.e. 'LED'
+        source_size (str): Source size, i.e. '1000um'
+        dist (str): Distance, i.e. '60mm'
+        baseline (str): Baseline, i.e. '127um'
+        pol (int, optional): Polarization. Defaults to None.
+        parts (list, optional): List of parts in experiment, in order of source to collector. Defaults to [].
+        parameters (dict, optional): Dictionary of experiment parameters. Defaults to {}.
+
+    Returns:
+        dict: Metadata to save in a yaml
+    """
+    # Establish metadata for a file
+    metadata = {}
+    metadata['caller'] = inspect.stack()[1].function # get caller function name
+    metadata['datetime'] = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+    metadata['parameters'] = params # {'snspd integration':'10ms', }
+    metadata['source'] = source #'LED'
+    metadata['source size'] = source_size #'1000um'
+    metadata['distance'] = dist #'60mm'
+    metadata['baseline'] = baseline #'127um'
+    metadata['polarization'] = pol #'0'
+    metadata['parts'] = parts # parts = ['LED', 'Collimating lens', 'Polarizer', 'SM VGA', 'Pol controllers', 'optical delay lines', '50:50 BS', 'SNSPDs']
+    return metadata
+
+def save(campaign, data, metadata):
+    """ This function will save data, including metadata
+
+    Args:
+        campaign (str): Name of campaign, will be used for name of saved file
+        data (pandas dataframe): Dataframe of data
+        metadata (dict): Dictionary of experimental info and context (todays data, etc)
+    """
+    
+    path = 'C:\\Users\\josh\\OneDrive - UWA\\UWA\\PhD\\3. Data\\{}'.format(campaign.title())
+    isExist = os.path.exists(path)
+    if not isExist:
+        os.makedirs(path)
+        
+    os.chdir('C:\\Users\\josh\\OneDrive - UWA\\UWA\\PhD\\3. Data\\{}'.format(campaign.title()))
+    existing = []
+    files_existing = []
+    for file in glob.glob("*.parquet"):
+        files_existing.append(file)
+        existing.append(int(file.split('.')[0].split('_')[1])) #splitting on . gives you the name, splitting on _ gives you the number
+        
+    existing_max = 0
+    if len(existing) > 0:
+        existing_max = max(existing)
+
+    filename = '{}_{:05d}'.format(campaign.lower(), existing_max+1)
+    if filename in files_existing:
+        filename = '{}_{:05d}_error_duplicated_name'.format(campaign.lower(), existing_max+1)
+        
+    table = pa.Table.from_pandas(data)
+    pq.write_table(table, '{}.parquet'.format(filename))
+        
+    with open('{}.yaml'.format(filename), 'w') as file:
+        yaml.dump(metadata, file)
+        
+    print('Saved {} parquet and yaml'.format(filename))
+    return filename
+    
+def load(campaign, index):
+    os.chdir('C:\\Users\\josh\\OneDrive - UWA\\UWA\\PhD\\3. Data\\{}'.format(campaign.title()))
+
+    filename = '{}_{:05d}'.format(campaign.lower(), index)
+    
+    data = pd.read_parquet('{}.parquet'.format(filename), engine='pyarrow')
+    metadata = ''
+    with open('{}.yaml'.format(filename), 'r') as file:
+        try:
+            metadata = yaml.safe_load(file)
+            print(metadata)
+        except yaml.YAMLError as exc:
+            print(exc)
+    
+    return data, metadata
+
+#print(int('file_0001.parquet'.split('.')[0].split('_')[1]))
